bd0bfef1d1c365eb2f10ac1e58100d38a88834b3
sairosantos <sairoraoni@gmail.com>
2021-04-15 16:49:25 +0000

    new stuff, bloom filter, more neon, etc

--- surf-neon.cpp
+++ surf-neon.cpp
@@ -471,7 +471,7 @@ void specs (TrieNode* node){
     }
 }
 
-uint8x8_t dot_product (uint8_t* src, uint8x8_t idx){
+uint8x8_t dot_product (uint8_t* src, uint8x8_t idx, uint8x8_t mask_vec){
     uint8x8_t res, aux_vec;
     uint8_t min, i;
     uint8x8_t aux;
@@ -480,13 +480,25 @@ uint8x8_t dot_product (uint8_t* src, uint8x8_t idx){
     res = vdup_n_u8 (0);
     min = vminv_u8 (idx);
 
-    for (i = 0; i < min; i += 8){
-        aux = vld1_u8 (&src[i]);
-        for (int j = 0; j < 8; j++) printf ("aux[%d] = %d\n", i+j, aux[j]);
-        accum += vaddlv_u8 (aux);
+    i = 0;
+    for (int j = 0; j < 8; j++){
+        if (mask_vec[j] = 1){
+            accum = 0;
+            for (i = 0; i < idx[j] - 8; i += 8){
+                aux = vld1_u8 (&src[i]);
+                //for (int j = 0; j < 8; j++) printf ("aux[%d] = %d\n", i+j, aux[j]);
+                accum += vaddlv_u8 (aux);
+            }
+
+            aux = vld1_u8 (&src[i]);
+            for (; i <= idx[j]; i++) {
+                accum += aux[i%8];
+                //printf ("aux[%d] = %d\n", i, aux[i%8]);
+            }
+            res[j] = accum;
+        }
     }
 
-    printf ("accum = %d\n", accum);
     return res;
 }
 
@@ -592,13 +604,78 @@ void neon_load(){
         cout << " vector_d[" << i << "] = " << vector_d[i] << "\n";
     }
 
+    uint8x8_t vec_mul = vdup_n_u8 (255);
     uint8x8_t lookup_mask = vdup_n_u8 (1);
     for (int i = 0; i < 8; i++) printf ("lookup_mask[%d] = %d | ", i, lookup_mask[i]);
+    printf ("\n");
     uint8x8_t result_mask = vdup_n_u8 (0);
     result_mask = gather (d_isprefixkey, vector_a, lookup_mask);
-    for (int i = 0; i < 8; i++) printf ("result_mask[%d] = %d | ", i, result_mask[i]);
+    //for (int i = 0; i < 8; i++) printf ("result_mask[%d] = %d | ", i, result_mask[i]);
+    lookup_mask = dot_product (d_haschild, vector_a, result_mask);
+    for (int i = 0; i < 8; i++) printf ("lookup_mask[%d] = %d | ", i, lookup_mask[i]);
+    printf ("\n");
+    //lookup_mask = vmull_u8 (lookup_mask, vec_mul);
+    for (int i = 0; i < 8; i++) printf ("lookup_mask[%d] = %d | ", i, lookup_mask[i]);
+    printf ("\n");
+    lookup_mask = vadd_u8 (lookup_mask, vec_mul);
+    for (int i = 0; i < 8; i++) printf ("lookup_mask[%d] = %d | ", i, lookup_mask[i]);
+    printf ("\n");
+}
 
-    dot_product (d_haschild, vector_a);
+void bloom_filter (uint8_t* src, uint8_t* filter, int src_length) {
+    uint8x16x4_t load;
+    uint32x4_t h1, h2, h3;
+    uint32x4_t aux1, aux2;
+    uint32x4_t* neon_r;
+    neon_r = (uint32x4_t*) malloc (4 * sizeof (uint32x4_t));
+
+    h1 = vdupq_n_u32 (src_length % 5);
+    h2 = vdupq_n_u32 (src_length % 34);
+    h3 = vdupq_n_u32 (src_length % 12);
+
+    aux1 = vdupq_n_u32 (0);
+    aux2 = vdupq_n_u32 (0);
+    for (int i = 0; i < 4; i++) neon_r[i] = vdupq_n_u32 (0);
+    
+    for (uint32_t src_idx = 0; src_idx < src_length; src_idx += 16){
+        load = vld4q_u8 (&src[src_idx]);
+        for (int i = 0; i < 4; i++){
+            neon_r[i][0] = load.val[i][0];
+            neon_r[i][1] = load.val[i][1];
+            neon_r[i][2] = load.val[i][2];
+            neon_r[i][3] = load.val[i][3];
+        }
+    }
+
+    for (int i = 0; i < 4; i++){
+        aux1 = vshlq_n_u32 (h1, 5);
+        aux2 = vshrq_n_u32 (h1, 7);
+        aux1 = vaddq_u32 (aux1, aux2);
+        aux1 = vaddq_u32 (aux1, neon_r[i]);
+        h1 = veorq_u32 (h1, aux1);
+
+        aux1 = vshlq_n_u32 (h2, 5);
+        aux2 = vshrq_n_u32 (h2, 7);
+        aux1 = vaddq_u32 (aux1, aux2);
+        aux1 = vaddq_u32 (aux1, neon_r[i]);
+        h2 = veorq_u32 (h2, aux1);
+
+        aux1 = vshlq_n_u32 (h3, 5);
+        aux2 = vshrq_n_u32 (h3, 7);
+        aux1 = vaddq_u32 (aux1, aux2);
+        aux1 = vaddq_u32 (aux1, neon_r[i]);
+        h3 = veorq_u32 (h3, aux1);
+    }
+
+    for (int i = 0; i < 4; i++){
+        h1[i] = h1[i] % src_length;
+        cout << "h1[" << i << "] = " << h1[i];
+        h2[i] = h2[i] % src_length;
+        cout << " h2[" << i << "] = " << h2[i];
+        h3[i] = h3[i] % src_length;
+        cout << " h3[" << i << "] = " << h3[i];
+        cout << "\n";
+    }
 }
 
 int main(int argc, char **argv) {
@@ -661,7 +738,8 @@ int main(int argc, char **argv) {
     //test (argv[1]);
 
     char_vector_load();
-    neon_load();
+    //neon_load();
+    bloom_filter (char_vector, d_labels, 16);
 
     free (pos_level);
     free (count_level);
