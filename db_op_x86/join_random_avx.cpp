ff498e33d2dc3062948ff0207cfe650978187d89
Sairo Santos <sairoraoni@gmail.com>
2021-06-07 13:11:30 +0000

    reworked avx version

--- db_op_x86/join_random_avx.cpp
+++ db_op_x86/join_random_avx.cpp
@@ -15,7 +15,7 @@
 #include <vector>
 #include <bitset>
 
-#define VECTOR_SIZE 2048
+#define VECTOR_SIZE 16
 
 using namespace std;
 
@@ -57,7 +57,7 @@ uint32_t castDate2Int (string date){
     return result;
 }
 
-void loadDateColumn (uint32_t* data_vector, uint32_t v_size, string file_path, int column){
+void loadDateColumn (int* data_vector, uint32_t v_size, string file_path, int column){
     ifstream data(file_path);
     string line;
     vector<string> tokens;
@@ -71,7 +71,7 @@ void loadDateColumn (uint32_t* data_vector, uint32_t v_size, string file_path, i
     }
 }
 
-void loadIntegerColumn (uint32_t* data_vector, uint32_t v_size, string file_path, int column){
+void loadIntegerColumn (int* data_vector, uint32_t v_size, string file_path, int column){
     ifstream data(file_path);
     string line;
     vector<string> tokens;
@@ -85,22 +85,34 @@ void loadIntegerColumn (uint32_t* data_vector, uint32_t v_size, string file_path
     }
 }
 
-void bloom_chk (int32_t *input_keys, size_t input_size, int32_t *mask_factors, int32_t shift, int32_t *bloom_filter, int32_t* output, size_t *output_count){
+void bloom_chk (int32_t *input_keys, size_t input_size, int32_t *mask_factors, int32_t* shift_m, int32_t *bloom_filter, size_t bloom_filter_size, int32_t* output, size_t *output_count){
 	__mmask16 k = _mm512_int2mask (0xFFFF);
 	__m512i mask_1 = _mm512_set1_epi32(1);
 	__m512i mask_31 = _mm512_set1_epi32(31);
 	__m512i mask_0 = _mm512_set1_epi32(0);
 	__m512i key, val, fun;
 	__m512i mul_factors = _mm512_loadu_si512 ((__m512i*) mask_factors);
-	__m128i mask_shift = _mm_cvtsi32_si128 (shift);
+	__m512i shift_amounts = _mm512_loadu_si512 ((__m512i*) shift_m);
+
+	uint32_t* aux_vec1 = (uint32_t*) malloc (VECTOR_SIZE * sizeof(uint32_t));
 	size_t i = 0, j = 16, o = 0;
 	do {
 		key = _mm512_mask_loadu_epi32(key, k, &input_keys[i]);
 		i += j;
 		fun = _mm512_mask_xor_epi32(fun, k, fun, fun);
 		__m512i fac = _mm512_permutexvar_epi32(fun, mul_factors);
-		__m512i bit = _mm512_srl_epi32(_mm512_mullo_epi32(key, fac), mask_shift);
-		__m512i bit_div; //= _mm512_i32gather_epi32(_mm512_srli_epi32(bit, 5), bloom_filter, 4);
+		__m512i bit = _mm512_sllv_epi32(_mm512_mullo_epi32(key, fac), shift_amounts);
+		_mm512_storeu_si512 (aux_vec1, bit);
+		for (int i = 0; i < VECTOR_SIZE; i++) aux_vec1[i] %= bloom_filter_size;
+		bit = _mm512_loadu_si512 (aux_vec1);
+		__m512i bit_div = _mm512_srli_epi32(bit, 5);// = _mm512_i32gather_epi32(_mm512_srli_epi32(bit, 5), bloom_filter, 4);
+		_mm512_storeu_si512 (aux_vec1, bit_div);
+		for (int i = 0; i < VECTOR_SIZE; i++) {
+			printf ("aux_vec1[%d] = %u -> ", i, aux_vec1[i]);
+			aux_vec1[i] = bloom_filter[aux_vec1[i]];
+			printf ("%u\n", aux_vec1[i]);
+		}
+		bit_div = _mm512_loadu_si512 (aux_vec1);
 		__m512i bit_mod = _mm512_sllv_epi32(mask_1, _mm512_and_epi32(bit, mask_31));
 		k = _mm512_test_epi32_mask(bit_div, bit_mod);
 		fun = _mm512_add_epi32(fun, mask_1);
@@ -111,10 +123,7 @@ void bloom_chk (int32_t *input_keys, size_t input_size, int32_t *mask_factors, i
 		j = 0;
 		int aux = _mm512_mask2int(k);
 		j = _mm_popcnt_u32 (aux);
-		/*while (aux) {
-			aux &= (aux - 1);
-			j++;
-		}*/
+		*output_count += j;
 	} while (i + j <= input_size);
 }
 
@@ -122,46 +131,54 @@ int* bloom_create (int n, float p, size_t *size, size_t *functions){
     *size = ceil((n * log(p)) / log(1 / pow(2, log(2))));
     *functions = round((*size / n) * log(2));
 
-    //std::cout << *size << " bits.\n";
-    //std::cout << *functions << " functions.\n";
-
     int* bf = (int*) aligned_alloc (32, *size/32 * sizeof (int));
 
     return bf;
 }
 
 void bloom_set(int32_t* entries, size_t entries_size, int32_t* bloom_filter, size_t bloom_filter_size, int32_t* factors, int32_t* shift_m, size_t functions){
-    	__m512i facts = _mm512_loadu_si512((__m512i*) factors);
-	__m128i shift = _mm_cvtsi32_si128(32 - 7);
-	__mmask16 mask_k = _mm512_int2mask (0xFFFF);
+    __mmask16 mask_k = _mm512_int2mask (0xFFFF);
 	__m512i mask_31 = _mm512_set1_epi32(31);
 	__m512i mask_1 = _mm512_set1_epi32(1);
 	__m512i mask_0 = _mm512_set1_epi32(0);
-	// non-constant registers
-	__m512i key, val, fun, inv = _mm512_set1_epi32(1);
+	__m512i facts = _mm512_loadu_si512((__m512i*) factors);
+	__m512i shift = _mm512_loadu_si512((__m512i*) shift_m);
+	uint32_t *aux_vec1 = (uint32_t*) malloc (VECTOR_SIZE * sizeof(uint32_t));
+	uint32_t *aux_vec2 = (uint32_t*) malloc (VECTOR_SIZE * sizeof(uint32_t));
 
+    __m512i key, aux, fun, fac, bit, bit_div, bit_mod;
+	
     for (int i = 0; i < entries_size; i += VECTOR_SIZE){
-	        __m512i new_key = _mm512_mask_loadu_epi32(key, mask_k, &entries[i]);
-		// reset old items
-		fun = _mm512_andnot_si512(inv, fun);
-		key = _mm512_andnot_si512(inv, key);
-		// pick hash function
-		__m512i fact = _mm512_permutexvar_epi32(facts, fun);
-		fun = _mm512_add_epi32(fun, mask_1);
-		// combine old with new items
-		key = _mm512_or_si512(key, new_key);
-		// hash the keys and check who is almost dmask_1
-		__m512i hash = _mm512_mullo_epi32(key, fact);
-		//__m512i last = _mm512_cmpeq_epi32(fun, mask_k);
-		hash = _mm512_srl_epi32(hash, shift); //hash result
-		// check bitmap
-		__m512i div_32 = _mm512_srli_epi32(hash, 5); //divide by 32, integer positions
-		div_32 = _mm512_i32gather_epi32(div_32, bloom_filter, 4); //gather integers
-		hash = _mm512_and_si512(hash, mask_31); //mod 31, bit position
-		hash = _mm512_sllv_epi32(mask_1, hash); //shift by that amount, this is the result
-		
-        	__m512i res = _mm512_or_si512(div_32, hash);
-		_mm512_i32scatter_epi32 (bloom_filter, div_32, res, 4);
+	    __m512i key = _mm512_mask_loadu_epi32(key, mask_k, &entries[i]);
+		for (int j = 0; j < functions; j++){
+            bit = key;
+            fac = _mm512_permutexvar_epi32(facts, fun);
+            bit = _mm512_mullo_epi32 (bit, fac);
+            bit = _mm512_sllv_epi32 (bit, shift);
+	    _mm512_storeu_si512	 (aux_vec1, bit);
+	    for (int i = 0; i < VECTOR_SIZE; i++) {
+		//printf ("%u ", aux_vec1[i]);
+		aux_vec1[i] %= bloom_filter_size;
+	    }
+	    //printf ("\n");
+            bit = _mm512_loadu_si512(aux_vec1);
+            bit_div = _mm512_srli_epi32 (bit, 5);
+            //bit_div = _mm512_i32gather_epi32 (bit_div, bloom_filter, 4);
+            bit_mod = _mm512_and_epi32 (bit, mask_31);
+            bit = _mm512_sllv_epi32 (mask_1, bit_mod);
+            //bit = _mm512_or_epi32 (bit_div, bit);
+
+	    _mm512_storeu_si512 (aux_vec1, bit);
+	    _mm512_storeu_si512 (aux_vec2, bit_div);
+	    for (int i = 0; i < VECTOR_SIZE; i++) {
+		//printf ("bloom_filter[%u] = %u\n", aux_vec2[i], bloom_filter[aux_vec2[i]]);
+		bloom_filter[aux_vec2[i]] = bloom_filter[aux_vec2[i]] | aux_vec1[i];
+		//printf ("bloom_filter[%u] = %u\n", aux_vec2[i], bloom_filter[aux_vec2[i]]);
+	    }
+	    //printf ("\n");
+            //_mm512_i32scatter_epi32 (bloom_filter, bit_div, bit, 4);
+            fun = _mm512_add_epi32 (mask_1, fun);
+        }
     }
 }
 
@@ -223,11 +240,11 @@ int main (__v32s argc, char const *argv[]){
 
     std::cout << "v_size = " << v_size*4 << "\n";
 
-    //loadDateColumn (o_orderkey, v_size, "/home/srsantos/Experiment/tpch-dbgen/data/orders.tbl", 1);
-    //loadDateColumn (l_orderkey, v_size * 4, "/home/srsantos/Experiment/tpch-dbgen/data/lineitem.tbl", 1);
+    //loadIntegerColumn (o_orderkey, v_size, "/home/srsantos/Experiment/tpch-dbgen/data/orders.tbl", 1);
+    //loadIntegerColumn (l_orderkey, v_size * 4, "/home/srsantos/Experiment/tpch-dbgen/data/lineitem.tbl", 1);
 
-    populate_vector (o_orderkey, (int) v_size/4);
-    populate_vector (l_orderkey, v_size);
+    populate_vector (o_orderkey, (int) v_size/4, 5);
+    populate_vector (l_orderkey, v_size, 6);
 
     size_t bloom_filter_size = 0;
     size_t hash_functions = 0;
@@ -238,6 +255,9 @@ int main (__v32s argc, char const *argv[]){
     int *output = (int*) malloc (v_size * sizeof (int));
     int *hash_function_factors = (int*) malloc (hash_functions * sizeof (int));    
     int *shift_amounts = (int*) malloc (hash_functions * sizeof (int));
+
+    std::cout << "bloom_filter_size = " << bloom_filter_size << "\n";
+    std::cout << "hash_functions = " << hash_functions << "\n";
 
     for (int i = 0; i < hash_functions; i++) {
         hash_function_factors[i] = prime_numbers[i % 15];
@@ -250,7 +270,10 @@ int main (__v32s argc, char const *argv[]){
     //bloom_chk (vector1, v_size, bloom_filter, bloom_filter_size, hash_function_factors, shift_amounts, hash_functions, output, &output_count);
     //std::cout << output_count << " positives.\n";
 
-    bloom_chk(l_orderkey, v_size, hash_function_factors, 5, bloom_filter, output, &output_count);
+    for (int i = 0; i < bloom_filter_size/32; i++) printf ("%u ", bloom_filter[i]);
+    printf ("\n");
+
+    bloom_chk(l_orderkey, v_size, hash_function_factors, shift_amounts, bloom_filter, bloom_filter_size, output, &output_count);
     std::cout << output_count << " positives.\n";
 
     //bloom_confirm (output, output_count, o_orderkey, v_size);
@@ -266,3 +289,4 @@ int main (__v32s argc, char const *argv[]){
     free (hash_function_factors);
     free (shift_amounts);
 }
+
